13e8d34e803cb7f70926889639c46e2566e2053a
Murugan Nagarajan <murugan425@gmail.com>
2021-07-26 13:24:47 +0530

    Updated the keycloak realm as bearer only.

--- src/main/java/com/megam/security/mgmauth/config/KeyCloakSecurityConfig.java
+++ src/main/java/com/megam/security/mgmauth/config/KeyCloakSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
@@ -42,13 +43,15 @@ public class KeyCloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) {
 		KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+		keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
 		auth.authenticationProvider(keycloakAuthenticationProvider);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		super.configure(http);
-		http.authorizeRequests().anyRequest().authenticated();
+		http.authorizeRequests().anyRequest().permitAll();
+		http.csrf().disable();
 	}
 
 }
